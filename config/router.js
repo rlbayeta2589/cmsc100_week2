58891f2dda40bc49f6eb2aa23733aa0b95b2f3d6
rlbayeta2589 <usefakename@gmail.com>
2015-08-28 11:37:59 +0800

    Lecture on nodejs

---
+++ config/router.js
@@ -0,0 +1,19 @@
+var student  = require('./../controllers/student');
+var teacher  = require('./../controllers/teacher');
+
+
+module.exports = function(router){
+	router.route('/students')
+		.get(student.find)
+	;
+
+	router.route('/teachers')
+		.get(teacher.find)
+		.post(teacher.add)
+		.put(teacher.update)
+		.delete(teacher.remove)
+	;
+
+	return router;
+};
+
---
+++ controllers/teacher.js
@@ -0,0 +1,12 @@
+exports.find = function(req, res){
+	res.send('Find a teacher');
+};
+exports.add = function(req, res){
+	res.send('Create a teacher');
+};
+exports.update = function(req, res){
+	res.send('Update a teacher');
+};
+exports.remove = function(req, res){
+	res.send('Delete a teacher');
+};
